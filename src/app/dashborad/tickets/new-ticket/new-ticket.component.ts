4b63f8de21b4d6e990ed0c9524a172e9bc66f1b0
Farid Sattarov <feridsattarov5@gmail.com>
2024-10-22 16:06:56 +0400

    viewChild and contentChild

--- src/app/dashborad/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashborad/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -10,7 +10,11 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.scss'
 })
 export class NewTicketComponent {
+  @ViewChild('form') private form?:ElementRef<HTMLFormElement>;
+  //private form2 = viewChild.required<ElementRef<HTMLFormElement>>('form')
   onSubmit(title:string,text:string){
     console.log(title,text)
+    this.form?.nativeElement.reset()
+    //this.form2().nativeElement.reset()
   }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, inject, Input, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,17 @@ import { Component, Input, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.scss',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)' : 'onClick()'
   }
 })
 export class ControlComponent {
   @Input({ required: true }) label!: string;
+  private el = inject(ElementRef)
+  @ContentChild('input') private control?:ElementRef<HTMLInputElement | HTMLTextAreaElement>
+  onClick(){
+    console.log(this.el.nativeElement)
+    console.log("click")
+    console.log(this.control)
+  }
 }
